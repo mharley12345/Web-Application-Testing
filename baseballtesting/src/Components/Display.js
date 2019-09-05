65f74180bde2b34e5a8bff4467aaaa4f16be1201
Mike Harley <mharley12345@gmail.com>
2019-09-05 12:12:55 -0700

    creates Dashboard , Display components defines display

---
+++ baseballtesting/src/Components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Display = () =>{
+
+
+return (
+
+    <div className="display">
+      <h2>Strikes</h2>
+      <h2>Balls</h2>
+      <h2>Outs</h2>
+    </div>
+)
+}
+export default Display 
